63e33d07756ca953f2fac7f30602868cff69bf8f
Sze Wong <swong@zerionsoftware.com>
2016-02-19 20:00:37 -0500

    Change to Canvas mobile so it runs on iOS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '');
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, '');
 
 game.state.add('Boot', ZenvaRunner.Boot);
 game.state.add('Preloader', ZenvaRunner.Preload);
